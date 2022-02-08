6c75a875b8f301ccb42f926bb72d3d5694846f17
gormlabenz <gorm-labenz@hotmail.com>
2022-02-08 07:56:28 +0100

    recreated main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gaugan2_renderer import Gaugan2Renderer
 
 renderer = Gaugan2Renderer(waiting_time=10)
-renderer.run("./input_folder", "./output_folder_2")
-# renderer.create_video("./output.mp4")
+renderer.run("./input_folder", "./output_folder")
+renderer.create_video("./output.mp4")
